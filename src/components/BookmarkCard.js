849605271c96d954b03251517fb08f574c13c06e
shirin <shirin.jain@iitg.ac.in>
2022-01-02 18:43:29 +0530

    bookmarks, login added

---
+++ src/components/BookmarkCard.js
@@ -0,0 +1,186 @@
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react'
+import PropTypes from "prop-types";
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardFooter,
+    CCardGroup,
+    CCardHeader,
+    CCardImage,
+    CCardLink,
+    CCardSubtitle,
+    CCardText,
+    CCardTitle,
+    CListGroup,
+    CListGroupItem,
+    CNav,
+    CNavItem,
+    CNavLink,
+    CCol,
+    CRow,
+  } from '@coreui/react'
+import Bookmarks from 'src/views/bookmarks/Bookmarks';
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     maxWidth: 345,
+//   },
+//   media: {
+//     height: 140,
+//   },
+//   modal: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+    
+//   },
+//   paper: {
+//     backgroundColor: theme.palette.background.paper,
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//     width:'400px',
+//     height:'200px'
+//   },
+// }));
+
+const Bookmark = ({book}) => {
+    
+//   const classes = useStyles();
+  
+//   const [open, setOpen] = useState(false);
+//   const [isCreateNewTimeline , setIsCreateNewTimeline] = useState(false);
+//   const [isAddToTimeline, setIsAddToTimeline] = useState(false);
+
+//   const handleDelete = ()=>{
+//     del('http://127.0.0.1:8000/bookmarksection/bookmarkApi/'+book.id+'/').then(()=>{
+//       console.log("deleted 🤔")
+//       window.location.reload();
+//     })
+  
+// }
+
+//   const handleOpen = () => {
+//     setOpen(true);
+//     setIsAddToTimeline(false);
+//     setIsCreateNewTimeline(false);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+//   const url = book.url_field
+//   const urlpage = () =>{
+
+//     window.open(url, "_blank")
+//   }
+  return (
+    // <Grid
+    //   item
+    //   md={3}
+    //   style={{
+    //     width: "400px",
+    //     height: "400px",
+    //     margin: "10px 10px 10px 10px",
+    //     paddingBottom: "20px",
+    //   }}
+    // >
+    //   <Card
+    //     className={classes.root}
+    //     style={{ width: "400px", height: "400px" }}
+    //   >
+    //     {window.location.pathname == "/explore"?(
+    //       <></>
+    //     ):(<DeleteIcon  
+    //       onClick = {handleDelete}
+    //       />)}
+        
+        
+    //       <CardMedia
+    //         className={classes.media}
+    //         image={book.image_field}
+    //         title={book.title_name}
+    //       />
+          
+    //       <div><h3 className="bookmark_title">{book.title_name}</h3></div>
+    //       <div><p className="bookmark_description">{book.description}</p></div>
+    //       <Button onClick={urlpage} className="button-url" size="small" color="primary">
+    //         Visit Page
+    //       </Button>
+    //     <div onClick={handleOpen} className="timelineadd">Add to your Timeline</div>
+    //     {/* <CreateNewTimeline id={book.id} date = {book.date} />
+    //      */}
+    //     <Modal
+    //     aria-labelledby="transition-modal-title"
+    //     aria-describedby="transition-modal-description"
+    //     className={classes.modal}
+    //     open={open}
+    //     onClose={handleClose}
+    //     closeAfterTransition
+    //     BackdropComponent={Backdrop}
+    //     BackdropProps={{
+    //       timeout: 500,
+    //     }}
+    //   >
+    //     <Fade in={open}>
+    //       <div className={classes.paper}>
+    //         {/* <h2 id="transition-modal-title">Transition modal</h2>
+    //         <p id="transition-modal-description">react-transition-group animates me.</p> */}
+            
+    //        {
+    //          !isAddToTimeline && !isCreateNewTimeline ?(
+    //             <div className="custom-modal">
+    //           <Button onClick={()=>{setIsCreateNewTimeline(false);setIsAddToTimeline(true)}} style={{color:'blue',padding:'15px',fontWeight:'normal'}}>Add to existing timeline</Button>
+    //         <Button onClick={()=>{setIsCreateNewTimeline(true);setIsAddToTimeline(false)}} style={{color:'blue',padding:'15px',fontWeight:'normal'}}>Create New Timeline</Button>
+    //         </div>
+    //          ): isCreateNewTimeline?(
+    //          <CreateNewTimeline id={book.id}/>
+    //          ): isAddToTimeline?(
+    //            <AddToTimeline id={book.id}/>
+    //          ):(
+    //            <></>
+    //          )
+    //        }
+    //       </div>
+    //     </Fade>
+    //   </Modal>
+    //   </Card>
+    // </Grid>
+    <CCol xs>
+    <CCard>
+    <CCardImage orientation="top" src={book.image_field} style={{width:'300px',height:'200px'}}/>
+    <CCardBody>
+      <CCardTitle style={{
+           maxWidth: '100%',
+           display: '-webkit-box',
+           WebkitBoxOrient: 'vertical',
+           WebkitLineClamp: 2,
+           overflow: 'hidden',
+           textOverflow: 'ellipsis',
+      }}>{book.title_name}</CCardTitle>
+      <CCardText style={{
+  maxWidth: '100%',
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 3,
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+}}>
+        {book.description}
+      </CCardText>
+    </CCardBody>
+    <CCardFooter>
+      <small className="text-medium-emphasis">Added on {book.date}</small>
+    </CCardFooter>
+  </CCard>
+  </CCol>
+  );
+};
+
+Bookmark.propTypes = {
+    book : PropTypes.object.isRequired
+  };
+
+export default Bookmark;
--- src/views/bookmarks/Bookmarks.js
+++ src/views/bookmarks/Bookmarks.js
@@ -1,21 +1,53 @@
 /* eslint-disable prettier/prettier */
-import React, {useEffect,useState} from 'react'
-import {get} from '../../utilities/util'
+import React, { useEffect, useState } from 'react'
+import { get } from '../../utilities/util'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardGroup,
+  CCardHeader,
+  CCardImage,
+  CCardLink,
+  CCardSubtitle,
+  CCardText,
+  CCardTitle,
+  CListGroup,
+  CListGroupItem,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CCol,
+  CRow,
+} from '@coreui/react'
+import { DocsCallout, DocsExample } from 'src/components'
+import ReactImg from 'src/assets/images/react.jpg'
+import BookmarkCard from '../../components/BookmarkCard'
 
 
+const Bookmarks = () => {
+  const apiURL = "http://127.0.0.1:8000/bookmarksection/bookmarkApi/";
+  const [bookmarks, setBookmarks] = useState(null);
+  useEffect(() => {
+    get(apiURL).then(res => {
+      setBookmarks(res);
+    })
+  }, []);
+  return (<>
 
-const Bookmarks = ()=>{
-    const apiURL = "http://127.0.0.1:8000/bookmarksection/bookmarkApi/";
-    const [bookmarks,setBookmarks] = useState(null);
-    useEffect(() => { 
-        get(apiURL).then(res=>{
-        setBookmarks(res);
-        console.log("hi")
-        console.log(res)
-        })
-      },[]);
-    return (<>
-    <h1>Bookmarks</h1>
-    </>);
+
+    <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 4 }}>
+      
+        {bookmarks && bookmarks.map((book, index) => (
+          <div key={book.id} style={{marginTop:'35px'}}>
+            <BookmarkCard book={book} />
+          </div>
+        ))}
+
+      
+    </CRow>
+
+  </>);
 }
 export default Bookmarks;

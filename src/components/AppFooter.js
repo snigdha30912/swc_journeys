5c6e32126d2b8852dc1b4bd056cf60a58e7ffd95
sniggo jain <snigdha.jain30@gmail.com>
2022-01-03 12:34:00 +0530

    added footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 import { CFooter } from '@coreui/react'
 
@@ -5,16 +6,9 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
-        </a>
-        <span className="ms-1">&copy; 2021 creativeLabs.</span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI for React
-        </a>
+        <p>Created by <b>SWC</b></p>
       </div>
     </CFooter>
   )

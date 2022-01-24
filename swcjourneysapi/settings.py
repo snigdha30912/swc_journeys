992d29a712df3911789ae1d98883ee1935b053ef
sniggo jain <snigdha.jain30@gmail.com>
2022-01-24 21:25:15 +0530

    settings changed

--- swcjourneysapi/settings.py
+++ swcjourneysapi/settings.py
@@ -165,6 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
